3d3e69c1b4bc62918fe30ae7a3b727155db15130
Carlos Alan Guerrero Belmonte <jupiterguerrero92@gmail.com>
2020-05-24 18:48:03 -0500

    se agrego la funcion de agregar empleados en la clase empleado

---
+++ Clases/Clase_Empleado.py
@@ -0,0 +1,18 @@
+class Empleado() :
+     def __init__(self, idEmpleado, nombre, direccion): 
+         self.idEmpleado = idEmpleado 
+         self.nombre = nombre 
+         self.direccion = direccion
+
+
+    def agregarEmpleado (self, idEmpleado, nombre, direccion):
+         print (30 * "*")
+         archivo = open("./archivos/Empleado.txt",'a')
+         archivo.write (self.idEmpleado)
+         archivo = open("./archivos/Empleado.txt",'a')
+         archivo.write (nombre)
+         archivo = open("./archivos/Empleado.txt",'a')
+         archivo.write (direccion)
+         archvios.close()
+        
+
