aacb8f52b7289f9e612c3c76bcc876b2204b01c6
aldo1871 <aaldoo10@hotmail.com>
2020-05-25 19:12:46 -0500

    se agrego codigo en la clase de tema

---
+++ Clases/Clase_Tema.py
@@ -0,0 +1,29 @@
+class Tema():
+    def __init__(self,idTema,nombre):
+        self.idTema = idTema
+        self.nombre = nombre
+
+    def AgregarTema(self,idTema,nombre):
+        archivo = open("./archivos/Tema.txt",'a')
+        
+        idTema = int(input("Ingresa el numero asignado al tema: "))
+        nombre = input("Ingresa el nombre del tema: ")
+
+        archivo.write(idTema + "|"+ nombre + "|" + idTema)
+
+        archivo.close()
+
+    def consultaTema (self):
+        archivo = open ("./archivos/Tema.txt",'r')
+        for renglon in archivo:
+            datosTema = renglon.split ('|')
+            print(f"Id: {datosTema[0]} Nombre: {datosTema[1]}")
+        archivo.close()
+
+    def consultaEspTema (self,idTema):
+        archivo = open ("./archivos/Tema.txt",'r',encoding='utf8')
+        for renglon in archivo:
+            print(f"No. Caracteres:{len(renglon)}: Renglon: {renglon} ")
+        archivo.close()
+        
+    
