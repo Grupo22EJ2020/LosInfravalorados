acbdb03aa22b950b6f731ee0289981b07336f92d
Orlando Mendoza Campos <orlando.mendozacm@uanl.edu.mx>
2020-05-26 19:22:36 -0500

    Se agrego la clase consultaCurso en Clase_Curo.py

--- Clases/Clase_Curso.py
+++ Clases/Clase_Curso.py
@@ -3,9 +3,17 @@ class Curso() :
          self.idCurso = idCurso 
          self.descripcion = descripcion 
          self.idEmpleado = idEmpleado
+         
 
      def AgregarCurso(self, idCurso, descripcion, idEmpleado): 
          archivo = open("./archivos/Curso.txt",'a')
          numeroInt= str (idCurso)
          archivo.write (numeroInt + "|" + descripcion + "|" + idEmpleado)
          archivo.close()
+
+
+     def consultaCurso (self,idCurso): 
+         archivo = open('./archivos/Cursos.txt',"r",encoding='utf8')
+         for renglon in archivo:
+             print(f"No. Caracteres: {len(renglon)}: Renglon: {renglon}")
+         archivo.close()  
